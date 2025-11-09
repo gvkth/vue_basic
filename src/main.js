9066bc4e688c56a676616c8365d01e1de358b867
DELL <vokiencuong@gmail.com>
2025-11-09 13:25:22 +0700

    run_vue_project_setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-new Vue({
-  el: '#app',
-  router,
-  store,
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router'; // nếu không có router, xóa dòng này
+// import store from './store'; // nếu có Vuex, bật dòng này
+
+Vue.config.productionTip = false;
+
+var appVue = new Vue({
+  router, // xóa nếu bạn không dùng router
+  // store, // bật nếu có store
   render: h => h(App)
-});
+}).$mount('#app');
+
+console.log(appVue)
