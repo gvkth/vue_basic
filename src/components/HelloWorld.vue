0b3d4fdf247c4f83b29e28443a0be8e00fefd7df
DELL <vokiencuong@gmail.com>
2025-11-10 13:49:40 +0700

    Demo data, method, v-mode, v-for

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,80 @@
 <template>
   <div class="hello">
-    <h1>Hello World!</h1>
+    <h1>{{ title }}</h1>
     <p>Welcome to your Vue.js app.</p>
+    <h2>{{ message }}</h2>
+    
+    <div class="counter-section">
+      <p>Count: {{ count }}</p>
+      <p>Double Count: {{ doubleCount }}</p>
+      <button @click="increment">Increment</button>
+      <button @click="decrement">Decrement</button>
+    </div>
+
+    <div class="todo-section">
+      <input v-model="newTodo" @keyup.enter="addTodo">
+      <button @click="addTodo">Add Todo</button>
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+          <button @click="removeTodo(index)">X</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  
+  // Data properties
+  data() {
+    return {
+      title: 'Vue Component Demo',
+      message: 'This is a simple Vue component.',
+      count: 0,
+      newTodo: '',
+      todos: []
+    }
+  },
+
+  // Computed properties
+  computed: {
+    doubleCount() {
+      return this.count * 2
+    }
+  },
+
+  // Methods
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+    addTodo() {
+      if (this.newTodo.trim()) {
+        this.todos.push(this.newTodo)
+        this.newTodo = ''
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  },
+
+  // Lifecycle hooks
+  created() {
+    console.log('Component created')
+  },
+  mounted() {
+    console.log('Component mounted')
+  },
+  beforeDestroy() {
+    console.log('Component will be destroyed')
+  }
 }
 </script>
 
@@ -15,5 +82,31 @@ export default {
 .hello {
   text-align: center;
   margin-top: 20px;
+}
+
+.counter-section {
+  margin: 20px 0;
+  padding: 20px;
+  background-color: #f5f5f5;
+}
+
+.todo-section {
+  margin: 20px 0;
+  padding: 20px;
+  background-color: #eef;
+}
+
+button {
+  margin: 0 5px;
+  padding: 5px 10px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 5px 0;
 }
 </style>
